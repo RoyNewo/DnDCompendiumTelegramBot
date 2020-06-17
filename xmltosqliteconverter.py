713021173d8442156b01ff920649188131260e35
RoyNewo <newo1310@gmail.com>
2020-06-17 20:21:20 +0200

    better terminal output, also database integrations

--- xmltosqliteconverter.py
+++ xmltosqliteconverter.py
@@ -1,5 +1,9 @@
 
 import xml.etree.ElementTree as ET
+import sqlite3 as lite
+import glob
+import sys
+# Creamos unas clase para poder manejar variables de una manera global en las funciones
 class my_str:
     def __init__(self, my_string):
         self.str_as_list = [my_string]
@@ -16,7 +20,7 @@ class my_str:
     def __str__(self):
         return self.str_as_list[0]
 
-
+# Funciones creadas para asignar valores y tambien para poder llamar a las funciones en una manera al estilo switch case
 def nombreHechizo(lista, contador):
     Name.change_str(lista)
 def nivelHechizo(lista, contador):
@@ -43,10 +47,17 @@ def descripcionHechizo(lista, contador):
         text.change_str(str(text) + lista)
     else:
         text.change_str(str(text) + "\\n ")
-def dadoHechizo(lista, contador):    
-    print(text)
-    print(roll)
-    print("roll ", contador.string() ,": ", lista)
+        
+def dadoHechizo(lista, contador):
+    #Pasamos la cadena a una variable de cadena, luego partimos el string con split se crea una lista, y con un bucle hacemos un print
+    texto = str(text)
+    dado = str(roll)
+    for linea in range(len(str(text).split('\\n '))):
+        print(texto.split('\\n ')[linea])
+    for linea2 in range(len(str(roll).split('\\n '))):
+        print(dado.split('\\n ')[linea2])
+
+    print("roll ", contador.string() ,": ", str(lista).split('\\n')[0])
     opcion = int(input("1 - attackroll, 2 - damage1, 3 - damage2, 4 - damage3, 5 - damage4, 6 - slotdamage: "))
     if opcion == 1:
         attackroll.change_str(lista)
@@ -61,6 +72,8 @@ def dadoHechizo(lista, contador):
     if opcion == 6:
         slotdamage.change_str(lista)
     l.change_str(l.my_int() + 1)
+
+# Vaiables globales, y diccionario para llamar a las funciones
 
 switcher = {'name': nombreHechizo, 'level': nivelHechizo, 'school': escuelaHechizo, 'ritual': ritaulHechizo, 'time': tiempoHechizo, 'range': rangoHechizo, 'components': componentesHechizo, 'duration': duracionHechizo, 'classes': claseHechizo, 'text': descripcionHechizo, 'roll': dadoHechizo}
 Name = my_str("")
@@ -83,57 +96,68 @@ roll = my_str("")
 l = my_str(0)
 
 def main():
-    tree = ET.parse('Compendiums\\Spells Compendium 1.3.0.xml')
-    root = tree.getroot()
-    '''
-    print(root[1][0].tag, root[1][0].text)
-    print(len(root))
-    print(len(root[0]))
-    '''
+    database = []
+    spell = []
     i = 0
     j = 0
     k = 0
-    database = []
-    spell = []
+    tree = None
+    root = None
+    # para coger varios archivos utilizamos la libreria glob y recorremos los ficheros
+    filenames = glob.glob("DnDCompendiumTelegramBot\\Spells\\*.xml")
+    for filename in filenames:
+        with open(filename, 'r', encoding="utf-8") as content:
 
-    for i in range(len(root)):
-        Name.change_str("")
-        Level.change_str(0)
-        school.change_str("")
-        ritual.change_str(False)
-        time.change_str("")
-        Range.change_str("")
-        components.change_str("")
-        duration.change_str("")
-        classes.change_str("")
-        text.change_str("")
-        attackroll.change_str("")
-        damage1.change_str("")
-        damage2.change_str("")
-        damage3.change_str("")
-        damage4.change_str("")
-        slotdamage.change_str("")
-        roll.change_str("")
-        l.change_str(0)
-        k = 0        
-        for x in root[i].findall("./roll"):            
-            roll.change_str( str(roll) + "roll 0" + str(k) + ": " + x.text + "\\n")
-            k += 1
+            tree = ET.parse(content)
+            root = tree.getroot()
+            i = 0
+            j = 0
+            k = 0            
+            for i in range(len(root)):
+                Name.change_str("")
+                Level.change_str(0)
+                school.change_str("")
+                ritual.change_str(False)
+                time.change_str("")
+                Range.change_str("")
+                components.change_str("")
+                duration.change_str("")
+                classes.change_str("")
+                text.change_str("")
+                attackroll.change_str("")
+                damage1.change_str("")
+                damage2.change_str("")
+                damage3.change_str("")
+                damage4.change_str("")
+                slotdamage.change_str("")
+                roll.change_str("")
+                l.change_str(0)
+                k = 0        
+                for x in root[i].findall("./roll"):            
+                    roll.change_str( str(roll) + "roll " + str(k) + ": " + x.text + " \\n ")                    
+                    k += 1
 
-        for j in range(len(root[i])):
-            #print(root[i][j].tag)
-            switcher[root[i][j].tag](root[i][j].text, l)
+                for j in range(len(root[i])):
+                    #print(root[i][j].tag)
+                    switcher[root[i][j].tag](root[i][j].text, l)
+                    j += 1
+                    
 
-            j += 1
-            
+                spell = [Name.string(),Level.my_int(),school.string(),ritual.my_bool(),time.string(),Range.string(),components.string(),duration.string(),classes.string(),text.string(),attackroll.string(),damage1.string(),damage2.string(),damage3.string(),damage4.string(),slotdamage.string()]
+                database.append(list(spell))
+                
+                '''
+                for ls in database:
+                    print(ls)
+                print(len(database))
+                spell.clear()
+                i += 1
+                '''
+    con = lite.connect('DnDCompendiumTelegramBot\\DnD 5e.db')
+    with con:
+        cur = con.cursor()
+        cur.executemany("INSERT INTO Spells VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",database)
 
-        spell = [Name.string(),Level.my_int(),school.string(),ritual.my_bool(),time.string(),Range.string(),components.string(),duration.string(),classes.string(),text.string(),attackroll.string(),damage1.string(),damage2.string(),damage3.string(),damage4.string(),slotdamage.string()]
-        database.append(list(spell))
-        for ls in database:
-            print(ls)
-        print(len(database))
-        spell.clear()
-        i += 1
 
 
     
